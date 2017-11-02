269e75e19fa6c2b797c0adc6458fa60ac7661031
bp117 <bindupriya117@gmail.com>
2017-11-02 11:11:14 +0530

    Update express1post.js

--- express1post.js
+++ express1post.js
@@ -1,5 +1,5 @@
 // bcrypt
-var bcrypt = require("bcrypt");
+//var bcrypt = require("bcrypt");
 
 // body-parser
 var bodyParser = require("body-parser");
@@ -8,59 +8,138 @@ var urlEncodedParser = bodyParser.urlencoded({extended: false});
 // express
 var express = require("express");
 var app = express();
+var path = require('path'),
+    fs = require('fs');
 
-//mysql
-var mysql = require("mysql");
-// connect strings for mysql
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "somePass",
-	database: "mysql"
-});
+/*var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    fs = require('fs');*/
 
-// connecting ......
-connection.connect();
+//var app = express();
 
-// requesting express to get data as text
-app.use(bodyParser.text());
+var db;
+
+var cloudant;
+
+
+
+var dbCredentials = {
+    dbName: 'my_sample_db'
+};
+
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
+var multipart = require('connect-multiparty')
+var multipartMiddleware = multipart();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+/*app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/style', express.static(path.join(__dirname, '/views/style')));*/
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+}
+
+function getDBCredentialsUrl(jsonData) {
+    var vcapServices = JSON.parse(jsonData);
+    // Pattern match to find the first instance of a Cloudant service in
+    // VCAP_SERVICES. If you know your service key, you can access the
+    // service credentials directly by using the vcapServices object.
+    for (var vcapService in vcapServices) {
+        if (vcapService.match(/cloudant/i)) {
+            return vcapServices[vcapService][0].credentials.url;
+        }
+    }
+}
+
+function initDBConnection() {
+    //When running on Bluemix, this variable will be set to a json object
+    //containing all the service credentials of all the bound services
+    if (process.env.VCAP_SERVICES) {
+        dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+    } else { //When running locally, the VCAP_SERVICES will not be set
+
+        // When running this app locally you can get your Cloudant credentials
+        // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
+        // Variables section for an app in the Bluemix console dashboard).
+        // Once you have the credentials, paste them into a file called vcap-local.json.
+        // Alternately you could point to a local database here instead of a
+        // Bluemix service.
+        // url will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
+        dbCredentials.url = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
+    }
+
+    cloudant = require('cloudant')(dbCredentials.url);
+
+    // check if DB exists if not create
+    cloudant.db.create(dbCredentials.dbName, function(err, res) {
+        if (err) {
+            console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
+        }
+    });
+
+    db = cloudant.use(dbCredentials.dbName);
+}
+
+initDBConnection();
+
+//app.get('/', routes.index);
 
 // using express for post method
 app.post("/somePage", urlEncodedParser, function(request, response) {
 	if(request.url!="/favicon.ico") {
 		if(request.body.regOrLogin=="Register") {
-			bcrypt.genSalt(10, function(err, salt) {
-				bcrypt.hash(request.body.pwd, salt, function(err, hash) {
-					var body = request.body;
+			
+var body = request.body;
 					var date = new Date();
 					var currentDate = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDay();
-					var postVars = {username: body.username, password: hash, dob: body.dob, reg_date: currentDate};
-					// insertion into MySQL
-					connection.query("INSERT INTO REG_NODEJS set ?", postVars, function(err, result) {
-						if(err) throw err;
-					});
-				});
-			});
-			console.log("user registered");
-			response.sendFile( "/js/nodejs/folder1/regSuccess.html");
+			    db.insert({
+        username: body.username,
+		password:body.pwd,
+		dob:body.dob,
+		reg_date:currentDate
+        
+    }, id, function(err, doc) {
+        if (err) {
+            console.log(err);
+            response.sendStatus(500);
+        } else
+           console.log("user registered");
+			response.sendFile( "regSuccess.html");
+       // response.end();
+    });
+			
 		} else if (request.body.regOrLogin=="Login") {
 			var  body = request.body;
 			console.log(body.username+": username");
-			connection.query("SELECT * FROM REG_NODEJS WHERE username='"+body.username+"'", function(err, res, fields){
-				if(err) { 
-					response.sendFile( "/js/nodejs/folder1/unauthorised.html");
-				} else {
-					bcrypt.compare(body.pwd, res[0].password, function(err, res) {
-						if(res) {
-							console.log("authorised user");
-							response.sendFile( "/js/nodejs/folder1/authorised.html");
-						} else {
-							console.log("not an authorised user");
-							response.sendFile( "/js/nodejs/folder1/unauthorised.html");
-						}
-					});
-				}
-			});
+
+			db.find({selector:{username:body.username}}, function(er, result) {
+  if (er) {
+    throw er;
+  }
+
+  console.log('Found %d documents with name Alice', result.docs.length);
+  for (var i = 0; i < result.docs.length; i++) {
+    console.log('  Doc id: %s', result.docs[i]._id);
+  }
+});
+			
 		}
 	}
 });
